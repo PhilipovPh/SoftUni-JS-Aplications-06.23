7ef90068bd8914f1b2f1be76036bb2c386296221
PhilipovPh <philipov.ph@gmail.com>
2023-07-09 13:58:02 +0300

    s

--- 2. Exercise Asynchronous Programming - 26.06.23/01.Bus-Stop/app.js
+++ 2. Exercise Asynchronous Programming - 26.06.23/01.Bus-Stop/app.js
@@ -1,3 +1,29 @@
-function getInfo() {
-    console.log("TODO...");
+async function getInfo() {
+    const stopId = document.getElementById('stopId').value;
+    const busesUl = document.getElementById('buses');
+    busesUl.textContent = '';
+
+    try {
+        const response = await fetch(`http://localhost:3030/jsonstore/bus/businfo/${stopId}`);
+
+        if (!response.ok) {
+            const error = new Error(response.statusText);
+            throw error;
+        }
+
+        const data = await response.json();
+
+        console.log(data);
+
+        document.getElementById('stopName').textContent = data.name;
+
+        Object.entries(data.buses).forEach(([busNumber, time]) => {
+            const busesLi = document.createElement('li');
+            busesLi.textContent = `Bus ${busNumber} arrives in ${time} minutes`
+            busesUl.appendChild(busesLi);
+        });
+    } catch (err) {
+        document.getElementById('stopName').textContent = `Error: ${err.message}`;
+    }
+
 }
